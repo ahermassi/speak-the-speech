233774095258d7dd752830152342008cf9b68485
Anouer Hermassi <hermassi.anouer@gmail.com>
2020-05-26 13:16:36 -0400

    Collect audio segments of each speaker

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import boto3
 import click
 from colorama import Fore, Style
 from pydub import AudioSegment
-from utils import text_to_speech
+from utils import text_to_speech, collect_audio_segments
 
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
@@ -29,6 +29,34 @@ def main(input, output_directory, separate=False):
         input_lines = f.read().splitlines()
 
     text_to_speech(input_lines, output_directory, polly, speech_lines, voices)
+
+    output_segments = {}  # This dictionary collects all the audio segments of each voice ID
+    for voice_id in voices:
+        output_segments[voice_id] = AudioSegment.silent(duration=0)
+
+    collect_audio_segments(output_segments, speech_lines, voices)
+
+    merged_segment = AudioSegment.silent(duration=0)
+    last_segment = None
+    now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    for voice_id, segment in output_segments.items():
+        output_audio_file = os.path.join(output_directory, '{}_{}.mp3'.format(now, voice_id))
+        if separate_audio_files:
+            print('Exporting TTS audio for {} of length {}ms to an MP3 at {}'.format(voice_id, len(segment),
+                                                                                     output_audio_file))
+            segment.export(output_audio_file, format='mp3')
+        if last_segment:
+            merged_segment = last_segment.overlay(segment)
+            last_segment = merged_segment
+        else:
+            last_segment = segment
+
+    output_audio_file = os.path.join(output_directory, now + '_Merged.mp3')
+    print('Exporting TTS audio to a single file at {}'.format(output_audio_file))
+    merged_segment.export(output_audio_file, format='mp3')
+
+    for speech_line in speech_lines:
+        os.remove(speech_line['path'])
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import sys
 from contextlib import closing
 from botocore.exceptions import BotoCoreError, ClientError
 from colorama import Fore, Style
+from pydub import AudioSegment
 
 
 def text_to_speech(input_lines, output_directory, polly, speech_lines, voices):
@@ -30,8 +31,8 @@ def text_to_speech(input_lines, output_directory, polly, speech_lines, voices):
             if 'AudioStream' in response:
                 # Closing the stream is important
                 with closing(response['AudioStream']) as audio_stream:
-                    current_voice_file_path = os.path.join(output_directory + voice_id + '.mp3')
-                    # With each synthesized speech line, we save the audio file along with the speaker name in
+                    current_voice_file_path = os.path.join(output_directory + '-' + voice_id + '.mp3')
+                    # With each synthesized speech line, we save the audio file path along with the speaker name in
                     # 'speech_lines' list
                     speech_lines.append({'path': current_voice_file_path, 'voice_id': voice_id})
                     try:
@@ -45,3 +46,30 @@ def text_to_speech(input_lines, output_directory, polly, speech_lines, voices):
                 # The response didn't contain audio data
                 print(Fore.RED + Style.BRIGHT + 'Could not stream audio.' + Style.RESET_ALL)
                 sys.exit()
+
+
+def collect_audio_segments(output_segments, speech_lines, voices):
+    total_speech_duration = 0
+    for speech_line in speech_lines:  # Each speech line is {voice_id: voice ID, path: audio file path}
+        current_voice_id = speech_line['voice_id']
+        path = speech_line['path']
+
+        print('Running total audio duration: {} '.format(total_speech_duration))
+        print('Current speaker: ' + current_voice_id)
+
+        segment = AudioSegment.from_mp3(path)  # Extract the audio segment from audio file
+        segment_length = len(segment)
+
+        for voice_id in voices:  # For each of the speakers, either append audio segment of their speech or a silence
+            # while someone else is speaking
+            if current_voice_id == voice_id:  # This is me speaking
+                print(voice_id + ': Appending segment from ' + path)
+                output_segments[voice_id] += segment
+            else:  # This is someone else speaking. I better be silent for the duration of speech
+                print('{}: Appending {}ms of silence to AudioSegment'.format(voice_id, segment_length))
+                output_segments[voice_id] += AudioSegment.silent(duration=segment_length)
+            # Add extra silence at the end of each clip for more natural-sounding conversation
+            output_segments[voice_id] += AudioSegment.silent(duration=450)
+
+        print()
+        total_speech_duration += segment_length
